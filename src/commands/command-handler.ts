05824608427ed0ff7362c1efad851d2201e8a1ec
Ivan Codasquieves <ivan@>
2021-10-12 19:18:59 -0300

    Fix command handler

--- src/commands/command-handler.ts
+++ src/commands/command-handler.ts
@@ -2,27 +2,26 @@ import { inject, injectable } from "inversify";
 import { ExecutionError } from "../domain-events/events/execution-error";
 import { EventPublisher } from "../domain-events/event-publisher";
 import { InvalidParametersError } from "../domain-events/events/invalid-parameters-error";
-import type { Result } from "../application/result";
 import type { Command } from "../commands/command";
 
 @injectable()
 export abstract class CommandHandler<T extends Command> {
   @inject(EventPublisher)
-  protected readonly notification!: EventPublisher;
+  protected readonly notifications!: EventPublisher;
 
   public async handle(command: T): Promise<void> {
     try {
       const [isInvalid, errors] = command.validate();
       if (isInvalid) {
-        this.notification.publish(new InvalidParametersError(errors));
+        this.notifications.publish(new InvalidParametersError(errors));
         return;
       }
 
       await this.execute(command);
     } catch (ex) {
-      this.notification.publish(new ExecutionError(ex as Error));
+      this.notifications.publish(new ExecutionError(ex as Error));
     }
   }
 
-  protected abstract execute(command: T): Promise<Result>;
+  protected abstract execute(command: T): Promise<void>;
 }
